e5e4e58ec9be8e7fa62c54c2890b51bd23163f20
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2019-11-21 16:41:44 +0300

    Scripted auto-commit on change (2019-11-21 16:41:44) by gitwatch.sh

--- lab4/Server.java
+++ lab4/Server.java
@@ -1,9 +1,11 @@
 
 
-
+import java.io.IOException;
 
 public class Server {
 
 
-    public static void main(String[] args) throws I
+    public static void main(String[] args) throws IOException {
+        ActorSystem system = ActorSystem.create("test")
+    }
 }
