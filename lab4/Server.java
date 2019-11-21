ebf645b305f6cdd198007588556851d338c4345c
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2019-11-21 23:29:05 +0300

    Scripted auto-commit on change (2019-11-21 23:29:05) by gitwatch.sh

--- lab4/Server.java
+++ lab4/Server.java
@@ -7,7 +7,22 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create("test")
-
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        MainHttp instance = new MainHttp(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
 
     }
 }
