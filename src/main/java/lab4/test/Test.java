38aab12d30af6da0ac1f2d9e70e004ed29056bbc
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2019-11-23 00:51:08 +0300

    Scripted auto-commit on change (2019-11-23 00:51:08) by gitwatch.sh

--- src/main/java/lab4/test/Test.java
+++ src/main/java/lab4/test/Test.java
@@ -23,14 +23,7 @@ public class Test {
     @JsonProperty(RESULT)
     private boolean result;
 
-    @JsonCreator
-     Test(@JsonProperty(TEST_NAME) String testName, @JsonProperty(EXPECTED_RESULT) String expectedResult,
-          @JsonProperty(PARAMS) ArrayList<Integer> params, @JsonProperty(RESULT) boolean result){
-        this.testName=testName;
-        this.expectedResult=expectedResult;
-        this.params=params;
-        this.result=result;
-    }
+
 
 
      Test(String testName, String expectedResult, ArrayList<Integer> params, boolean result){
