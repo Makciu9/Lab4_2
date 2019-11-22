8b80df65b2336dab3e083347d9476e5c3575fe42
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2019-11-22 19:39:47 +0300

    Scripted auto-commit on change (2019-11-22 19:39:47) by gitwatch.sh

--- lab4/Server.java
+++ lab4/Server.java
@@ -20,7 +20,8 @@ import akka.stream.javadsl.Flow;
 import lab4.Store.GetMessage;
 import lab4.Store.StoreActor;
 import lab4.Test.TestActor;
-import lab4.
+import lab4.Test.TestPackageActor;
+
 
 
 
