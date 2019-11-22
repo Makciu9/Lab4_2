195c7ac40c906de4310de1327c4766d6bbb56584
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2019-11-22 19:37:09 +0300

    Scripted auto-commit on change (2019-11-22 19:37:09) by gitwatch.sh

--- lab4/Server.java
+++ lab4/Server.java
@@ -7,6 +7,7 @@ import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
 
 
 import java.io.IOException;
