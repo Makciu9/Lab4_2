2efb81394f65799c6fc5dcf8887c1c798df27264
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2019-11-23 00:49:43 +0300

    Scripted auto-commit on change (2019-11-23 00:49:43) by gitwatch.sh

--- src/main/java/lab4/test/Test.java
+++ src/main/java/lab4/test/Test.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
-
+@JsonAutoDetect
 public class Test {
     private final String TEST_NAME = "testName";
     private final String EXPECTED_RESULT = "expectedResult";
