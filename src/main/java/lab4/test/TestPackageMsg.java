2e023be5946dc53b35698ef0deec6a872123f127
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2019-11-23 00:07:27 +0300

    Scripted auto-commit on change (2019-11-23 00:07:27) by gitwatch.sh

--- src/main/java/lab4/test/TestPackageMsg.java
+++ src/main/java/lab4/test/TestPackageMsg.java
@@ -22,7 +22,7 @@ public class TestPackageMsg {
     @JsonProperty(TESTS)
     private ArrayList<Test> tests;
 
-    public class TestPackageMsg (int packageID, String jsScript, String functionName,) {
+    public class TestPackageMsg (int packageID, String jsScript, String functionName, ArrayList<Test> tests) {
         this.packageID = packageID;
         this.jsScript = jsScript;
         this.functionName = functionName;
