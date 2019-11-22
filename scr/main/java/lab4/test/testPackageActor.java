eb25397615c071d46e74322b94e9579aff2f0a27
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2019-11-22 23:18:35 +0300

    Scripted auto-commit on change (2019-11-22 23:18:35) by gitwatch.sh

--- scr/main/java/lab4/test/testPackageActor.java
+++ scr/main/java/lab4/test/testPackageActor.java
@@ -7,7 +7,7 @@ import test.Test;
 import test.TestMsg;
 import test.TestPackageMsg;
 
-public class TestPackageActor extends AbstractActor {
+class TestPackageActor extends AbstractActor {
     private ActorSelection testPerformerRouter = getContext().actorSelection("/user/testPerformerRouter");
     public Receive createReceive() {
         return ReceiveBuilder.create()
